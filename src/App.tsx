c37b759564193f749e9f059c53d0483e6f83fbd0
thunder-volt <ishu114407@gmail.com>
2022-08-16 22:14:11 +0530

    map display done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+// import Map from './components/Map'
+import { Router, Switch, Route } from 'wouter'
+// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+
+import Home from './pages/Home';
 const App = () => {
-  return <div className='App'>CWL 2022</div>
+  return (
+    <>
+      <Router base='/'>
+        <Switch>
+          <Route
+            component={() => <Home />}
+          />
+          <Route
+          />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 export default App
