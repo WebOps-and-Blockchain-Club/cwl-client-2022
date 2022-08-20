d61db34e19cb856e026ca651b5a83bd48bcee9ad
thunder-volt <ishu114407@gmail.com>
2022-08-20 13:46:02 +0530

    markers added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Map from '../components/Map/Map'
 import '../styles/Maps/MapDisplay.css';
+import '../styles/Maps/Marker.css'
 const Home = () => {
     return (
         <div>
